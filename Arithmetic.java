e92e068a6e268dafd0b90b2343da888b4db5bc0f
Gaurav-Shende-93 <githubcontol@gmail.com>
2024-12-19 00:19:13 +0530

    this is program of Arithmetic Binary Operators

---
+++ Arithmetic.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Arithmetic {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number : ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter Second Number : ");
+        int b = sc.nextInt();
+
+        System.out.println("Addition is : " + (a + b));
+        System.out.println("Subtraction is : " + (a - b));
+        System.out.println("Multiplication is : " + (a * b));
+        System.out.println("Division is : " + (a / b));
+        System.out.println("Modulo is : " + (a % b));
+
+        sc.close();
+
+    }
+}
