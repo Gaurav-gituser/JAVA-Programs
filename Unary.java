6ea494b6cecb0e16bf387a300ae990cfc977d782
Gaurav-Shende-93 <githubcontol@gmail.com>
2024-12-19 00:55:32 +0530

    this is program of Arithmetic Unary Operators

---
+++ Unary.java
@@ -0,0 +1,38 @@
+public class Unary {
+
+    public static void main(String args[]) {
+
+        // Post Increment
+
+        int a = 10;
+        int b = a++;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        // Pre Increment
+
+        int d = 10;
+        int e = ++d;
+
+        System.out.println(d);
+        System.out.println(e);
+
+        // post Decrement
+
+        int h = 10;
+        int i = h--;
+
+        System.out.println(h);
+        System.out.println(i);
+
+        // pre Decrement
+
+        int j = 10;
+        int k = --j;
+
+        System.out.println(j);
+        System.out.println(k);
+
+    }
+}
