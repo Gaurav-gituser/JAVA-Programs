fbecf11a8271a771c7b8ca795cad29ce921441a2
Gaurav-Shende-93 <githubcontol@gmail.com>
2024-12-18 19:02:12 +0530

    this is program to show what is comments

---
+++ comments.java
@@ -0,0 +1,19 @@
+public class comments {
+
+    public static void main(String args[]) {
+
+        // this is a code to multiply to numbers
+
+        int a = 10;
+        int b = 20;
+        int sum = a * b;
+
+        System.out.println(sum);
+
+        /*
+         * this is multiple line comments
+         */
+
+    }
+
+}
