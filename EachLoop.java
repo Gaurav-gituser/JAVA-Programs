fc37049dd241e7d2d0a0ecb89b743fff608745b3
Gaurav-Shende-93 <githubcontol@gmail.com>
2025-02-12 14:17:55 +0530

    this is Each Loop Program

---
+++ EachLoop.java
@@ -0,0 +1,15 @@
+public class EachLoop {
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {1,2,3,4};
+
+        for (int num: arr){
+
+
+            System.out.println(num);
+
+        }
+    }
+}
