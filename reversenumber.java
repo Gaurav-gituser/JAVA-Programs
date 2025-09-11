416051e054c8b313a0f9049aebd740ec4894d583
Gaurav <githuncontol@gmail.com>
2025-09-11 12:04:35 +0530

    REverse number using while loop and some new concepts

---
+++ reversenumber.java
@@ -0,0 +1,16 @@
+public class reversenumber {
+
+  public static void main(String[] args) {
+
+    int num = 1234;
+
+    while (num > 0) {
+
+      System.out.print(num % 10);
+      num = num / 10;
+
+    }
+
+  }
+
+}
