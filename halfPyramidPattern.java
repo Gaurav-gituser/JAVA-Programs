4e51f8116e3c361749c0903b4bfb53fbcc28154a
Gaurav <githuncontol@gmail.com>
2025-09-11 16:41:31 +0530

    This is the half pyramid  pattern program

---
+++ halfPyramidPattern.java
@@ -0,0 +1,19 @@
+public class halfPyramidPattern {
+
+    public static void main(String args[]) {
+
+        int n = 10;
+
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
